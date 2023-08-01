e7675627b8ceb73cdb7d31622fa3352316001fb3
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-01 17:21:09 +0530

    Creating a stack and add elements

---
+++ StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class LinkedListStack
+    {
+        public Node top;
+
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("inserted value is {0}", data);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace StackAndQueue
+﻿using System.Collections.Generic;
+
+namespace StackAndQueue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stack And Queue Program");
+            LinkedListStack list = new LinkedListStack();
+            list.Push(56);
+            list.Push(30);
+            list.Push(70);
+            list.Display();
         }
     }
 }
