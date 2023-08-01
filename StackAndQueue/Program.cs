c8c4aaef6afd735ff43a7f187d7f828179ebadc6
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-01 17:14:26 +0530

    Creating anew stack and queue project and add it

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack And Queue Program");
+        }
+    }
+}
