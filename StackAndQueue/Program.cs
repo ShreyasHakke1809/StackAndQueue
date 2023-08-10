a0de20d7beacfcd1afc57434a30e7efb71600dc7
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-10 21:00:19 +0530

    Ability to create queue and add element

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -9,18 +9,19 @@ namespace StackAndQueue
         {
             Console.WriteLine("Welcome to the Stack And Queue Program");
             LinkedListStack list = new LinkedListStack();
+            Queue queue = new Queue();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("\n1.Create a stack and push elements\n2.Peek and pop");
+            Console.WriteLine("\n1.Create a stack and push elements\n2.Peek and pop\n3.Enqueue elements ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
-                case 1:
-                list.Push(56);
-                list.Push(30);
-                list.Push(70);
-                list.Display();
+                    case 1:
+                    list.Push(56);
+                    list.Push(30);
+                    list.Push(70);
+                    list.Display();
                     break;
-                case 2:
+                    case 2:
                     list.Push(56);
                     list.Push(30);
                     list.Push(70);
@@ -41,6 +42,12 @@ namespace StackAndQueue
                     {
                         Console.WriteLine("\nThe stack is empty");
                     }
+                    break;
+                    case 3:
+                    queue.Enqueue(56);
+                    queue.Enqueue(30);
+                    queue.Enqueue(70);
+                    queue.Display();
                     break;
             }
         }
---
+++ StackAndQueue/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Queue
+    {
+        public Node head;
+        public Queue() 
+        {
+            this.head = null;
+        }
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.head;
+            }
+            this.head = node;
+            Console.WriteLine("inserted value is {0}", data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
